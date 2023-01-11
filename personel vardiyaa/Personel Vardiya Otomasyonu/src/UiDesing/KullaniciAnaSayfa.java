8fd793f651edb82b23e4b79772efd7a6d1d78658
Kerem ÖNAL <114138735+kkeremonal@users.noreply.github.com>
2023-01-11 14:12:11 +0300

    Update KullaniciAnaSayfa.java

--- personel vardiyaa/Personel Vardiya Otomasyonu/src/UiDesing/KullaniciAnaSayfa.java
+++ personel vardiyaa/Personel Vardiya Otomasyonu/src/UiDesing/KullaniciAnaSayfa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UiDesing;
 
 import DataBase.db;
@@ -11,10 +7,7 @@ import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author huseyinkaradana
- */
+
 public class KullaniciAnaSayfa extends javax.swing.JFrame {
 
     String tcNo="";
@@ -102,7 +95,7 @@ public class KullaniciAnaSayfa extends javax.swing.JFrame {
     }
     
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+   
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -195,15 +188,11 @@ public class KullaniciAnaSayfa extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
     
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+       
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -220,9 +209,7 @@ public class KullaniciAnaSayfa extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(KullaniciAnaSayfa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new KullaniciAnaSayfa().setVisible(true);
@@ -230,9 +217,9 @@ public class KullaniciAnaSayfa extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+   
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tb_gorev;
     private javax.swing.JLabel txt_baslik;
-    // End of variables declaration//GEN-END:variables
+   
 }
